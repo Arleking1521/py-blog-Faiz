b60c032912ae86bc817a482907427c8d8ff895ce
Arleking <Abdukarim600@gmail.com>
2024-12-03 10:01:22 +0500

    Clearing

--- users/token.py
+++
@@ -1,8 +0,0 @@
-from .models import User
-from django.utils import six, timezone
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-
-class TokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user: User, timestamp):
-        login_time = timezone.datetime.timestamp(user.date_joined)
-        return six.text_type(user.pk) + six.text_type(timestamp)+ six.text_type(user.is_active)+six.text_type(login_time)
