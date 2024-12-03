ddb7da6ca68c77a8751d7a6e2a942624e8e491f8
Arleking <Abdukarim600@gmail.com>
2024-12-03 10:02:39 +0500

    Creating first project

---
+++ posts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Post, Post_Attachments, Comment
+from modeltranslation.admin import TranslationAdmin
+# Register your models here.
+
+
+# admin.site.register(Post)
+
+    
+@admin.register(Post)
+class PostAdmin(TranslationAdmin):
+    list_display=(
+        'author',
+        'title_ru',
+        'title_kk',
+        'title_en',
+        'content_ru',
+        'content_kk',
+        'content_en',
+        'date',
+        'edited',
+    )
+admin.site.register(Post_Attachments)
+admin.site.register(Comment)
