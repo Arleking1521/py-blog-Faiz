28eb809e1d54e4f3af3fb51636eeaa1c79b75bcc
Arleking <Abdukarim600@gmail.com>
2024-12-05 13:55:40 +0500

    Commit before deploy

--- posts/admin.py
+++ posts/admin.py
@@ -1,24 +1,28 @@
 from django.contrib import admin
 from .models import Post, Post_Attachments, Comment
 from modeltranslation.admin import TranslationAdmin
-# Register your models here.
+from django.utils.translation import gettext_lazy as _
 
-
-# admin.site.register(Post)
-
-    
 @admin.register(Post)
-class PostAdmin(TranslationAdmin):
-    list_display=(
-        'author',
-        'title_ru',
-        'title_kk',
-        'title_en',
-        'content_ru',
-        'content_kk',
-        'content_en',
+class CustomPostAdmin(TranslationAdmin):
+    fieldsets = (
+        (_('Author'), {'fields': ('author',)}),
+        (_('Information on russian'), {'fields': ('title_ru', 'content_ru',)}),
+        (_('Information on kazakh'), {'fields': ('title_kk', 'content_kk',)}),
+        (_('Information on english'), {'fields': ('title_en', 'content_en',)}),
+        (_('Additionaly information'), {'fields': ('edited',)}),
+    )
+    add_fieldsets = (
+        (_('Author'), {'fields': ('author',)}),
+        (_('Information on russian'), {'fields': ('title_ru', 'content_ru',)}),
+        (_('Information on kazakh'), {'fields': ('title_kk', 'content_kk',)}),
+        (_('Information on english'), {'fields': ('title_en', 'content_en',)}),
+    )
+    list_display = (
+        'title',
         'date',
         'edited',
     )
+
 admin.site.register(Post_Attachments)
 admin.site.register(Comment)
